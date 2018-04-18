1cf4d17e8005527cb14ec79b066426475684a220
MorganHJames <mojo@morganhjames.com>
2018-04-18 17:20:07 +0100

    Initalised the scene
    Added mug,plate,donut,temp desk, temp walls, floors and ceiling, along with a light hanging on by a spring joint

---
+++ Assets/Scripts/Smash.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Smash : MonoBehaviour
+{
+	public float breakVelocity;
+	public GameObject brokenPrefab;
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.GetComponent<Rigidbody>().velocity.magnitude > breakVelocity || GetComponent<Rigidbody>().velocity.magnitude > breakVelocity)
+		{
+			//Smash!
+			Destroy(gameObject);
+			GameObject smashedItem = Instantiate(brokenPrefab, transform.position, transform.rotation, transform.parent);
+			
+		}
+	}
+
+}
